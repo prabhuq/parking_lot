a055f63e92f4b575d65927c440e37355261c837c
Prabhu Pant <prabhupant09@gmail.com>
2020-02-06 20:36:24 +0530

    Add car model

---
+++ src/car.py
@@ -0,0 +1,12 @@
+class Car:
+    """
+    A class to define a car with three attributes
+    1- Registration ID
+    2- Slot Number
+    3- Color of the car
+    """
+    
+    def __init__(self, registration_id, slot, color):
+        self.registration_id = registration_id
+        self.slot = slot
+        self.color = color
